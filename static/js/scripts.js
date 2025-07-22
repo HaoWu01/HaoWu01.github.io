705800d5b6f72c4fae09ed83437910b88c0e4cf0
HaoWu01 <haowmas@gmail.com>
2025-07-22 14:08:23 +0800

    scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/';
 const config_file = 'config.yml';
-const section_names = ['home', 'publications', 'awards'];  // 添加 education
+const section_names = ['home', 'publications', 'awards'];
 
 window.addEventListener('DOMContentLoaded', event => {
 
